b36a440971cf8d023aa571c7eca1cdab61ba4968
Filip <43730405+Gecazo@users.noreply.github.com>
2020-08-20 16:58:22 +0300

    Update rockpaperscissors.py
    List has been replaced with tuple, some little fixes.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,10 +1,12 @@
 import random
-countD = 0 # Counter - Draw, Loose, Win
-countL = 0
-countW = 0
-l = ['r','s','p'] # Random item - rock, scissors, paper
-  
-def inp(): # Number of turns
+
+count_draws = 0  # Counter - Draw, Loose, Win
+count_loses = 0
+count_wins = 0
+TYPE_LIST = ('rock', 'scissors', 'paper')  # Items - rock, scissors, paper
+
+
+def inp():  # Number of turns
     global n
     while True:
         try:
@@ -12,32 +14,34 @@ def inp(): # Number of turns
             return n
         except ValueError:
             print("It's not a number!")
-  
-  
-def logic(x, y): # game logic
-    global countD, countW, countL
-        if x == y:
-            print('Draw')
-            countD += 1
-        elif (x == 'r' and y == 's') or (x == 's' and y == 'p') or (x == 'p' and y == 'r'):
-            print('Win')
-            countW += 1
-        else:
-            print('Lose')
-            countL += 1
-        return countD, countL, countW
-  
-# start
+
+
+def logic(x, y):  # Game logic
+    global count_draws, count_wins, count_loses
+    if x == y:
+        print('Draw')
+        count_draws += 1
+    elif (x == 'rock' and y == 'scissors') or (x == 'scissors' and y == 'paper') or (x == 'paper' and y == 'rock'):
+        print('Win')
+        count_wins += 1
+    else:
+        print('Lose')
+        count_loses += 1
+    return count_draws, count_loses, count_wins
+
+
+# Start
 inp()
 while n != 0:
     while True:
-        x = input('Rock (r), Scissors (s), Paper (p)')
-        if x not in l:
-            print('Enter r, s, p')
-        else: break
-  
-    y = random.choice(l)
+        x = input('Choose your item: rock , scissors , paper ')
+        if x not in TYPE_LIST:
+            print('Enter rock, scissors, paper')
+        else:
+            break
+
+    y = random.choice(TYPE_LIST)
     logic(x, y)
     n -= 1
 else:
-    print('Wins - {0}\nLoses - {1}\nDraws- {2}'.format(countW, countL, countD))
+    print('Wins - {0}\nLoses - {1}\nDraws- {2}'.format(count_wins, count_loses, count_draws))
